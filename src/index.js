f337b94a03248e239ee04b640452e3b1d85fd37f
Jude AlFarah <>
2025-08-03 18:53:18 +0200

    impl

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./style.css";
-import App from "./App";
+import AppV1 from "./AppV1";
+import AppV2 from "./AppV2";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <AppV1 /> */}
+    <AppV2 />
   </React.StrictMode>
 );
